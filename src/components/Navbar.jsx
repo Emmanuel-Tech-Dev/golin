96e4e383464ef1c20415dcfccd951609dc9e28f9
Emmanuel_Tech_Dev <65460128+Emmanuel-Tech-Dev@users.noreply.github.com>
2023-04-15 20:53:27 +0000

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,23 +11,37 @@ const Navbar = () => {
     setNav(!nav)
  }
 
+let links = [
+    {name: 'Home' , link: '/'},
+    {name: 'Services' , link: '/'},
+    {name: 'Our Products' , link: '/'},
+    {name: 'About us' , link: '/'}
+]
+
+
+
+
     return (
         <header>
             <nav className='text-white flex justify-between items-center m-5 md:px-[10%]'>
                 <label><img className='mt-1' src='logo.png'/></label>
                 
                 <ul className=' md:flex gap-10 text-md hidden z-30 md:gap-4 '>
-                <li className='p-3 text-md  hover:bg-zinc-400 hover:text-[#1e1e1e]'><a href='#'>Home</a></li>
-                    <li className='p-3 text-[#606060] hover:bg-zinc-400 hover:text-[#1e1e1e] text-md'><a href='#'>Services</a></li>
-                    <li className='p-3 text-[#606060]  hover:bg-zinc-400 hover:text-[#1e1e1e] text-md'><a href='#'>Our Projects</a></li>
-                    <li className='p-3 text-[#606060]  hover:bg-zinc-400 hover:text-[#1e1e1e] text-md'><a href='#'>About us</a></li>
+
+
+                  {  
+                  links.map((link) =>(
+                      <li className='p-3 text-md hover:bg-zinc-400 hover:text-[#1e1e1e]'><a href={link.link}>{link.name}</a></li>
+                  ))
+                
+                 }  
                </ul>
               
                <a href='#'><button className='hidden md:px-5 py-2 md:block outline-[#5454D4] border-2 border-[#b9b9b9] rounded-md  items-center gap-3'>Contact us</button></a>
    
 
                 <button onClick={handleNav} className='mt-2 cursor-pointer z-20 md:hidden ' >
-                    {!nav ? <IoClose size={20}/> : <HiMenuAlt3 size={20}/> }
+                    {!nav ? <HiMenuAlt3 size={20}/> :<IoClose size={20} className='fixed right-5 top-8'/>  }
                 </button>
             
            
@@ -35,16 +49,16 @@ const Navbar = () => {
         
            {/* MObile Menu */}
 
-               <div className={!nav ? 'fixed left-0 top-0 w-full  bg-[#1e1e1e] z-10 text-center ease-in-out duration-500 ' : ' fixed  top-[-100%]  '}>
+               <div className={!nav ? ' fixed  top-[-100%]  ' :'fixed left-0 top-0 w-full  bg-[#1e1e1e] z-10 text-center ease-in-out duration-500 '  }>
                <label><img className=' m-5 mt-6  md:hidden' src='./logo.png' /></label>
              
              
-                <ul className='flex mt-2 p-4 flex-col w-full md:hidden'>
+                <ul className='flex mt-2 p-4 flex-col md:hidden'>
                     <li className='p-4 text-md border-gray-800 border-b  hover:bg-zinc-400 hover:text-[#1e1e1e]'><a href='#'>Home</a></li>
                     <li className='p-4 text-[#606060] border-gray-800 border-b hover:bg-zinc-400 hover:text-[#1e1e1e] text-md'><a href='#'>Services</a></li>
                     <li className='p-4 text-[#606060] border-gray-800 border-b hover:bg-zinc-400 hover:text-[#1e1e1e] text-md'><a href='#'>Our Projects</a></li>
                     <li className='p-4 text-[#606060] border-gray-800 border-b hover:bg-zinc-400 hover:text-[#1e1e1e] text-md'><a href='#'>About us</a></li>
-                      <a href='#'><button className='px-5 py-2 outline-[#5454D4] border-2 border-[#b9b9b9] rounded-md flex items-center gap-3'>Contact us</button></a>
+                      <a href='#'><button className='hidden px-5 py-2 outline-[#5454D4] border-2 border-[#b9b9b9] rounded-md md:flex items-center gap-3'>Contact us</button></a>
     </ul>
 
          
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,39 +1,53 @@
-import React from 'react'
-import { GrFormPrevious , GrFormNext } from 'react-icons/gr';
+import React, { useState } from 'react';
+import { GrFormPrevious, GrFormNext } from 'react-icons/gr';
+
 const Work = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = ['./carousel.png', './image2.png', './image3.png'];
+
+  const handlePrevClick = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+   
+  };
+
   return (
     <div>
-      
       <div className="content text-[#fefefe] m-5 md:mt-20 md:px-[10%]">
-      <h1 className='text-3xl font-bold  py-10 pb-5 md:text-[48px]'>Our Awesome Portfolio</h1>
-      <img className='ml-[90%] w-[34px]' src='./Circles.png'/>
-      
-      {/* Caurousel Cards and Controllers Here */}
-     <div className="works py-5 m-auto ">
-<div className="images  mx-14 md:flex md:gap-20 md:mx-0 ">
-    <img src='./carousel.png'/>
-    <img className='hidden md:block' src='./image2.png'/>
-    <img className='hidden md:block ' src='./image3.png'/>
-</div>
-<div className="controllers flex mt-10  justify-center gap-3">
-    <div className="prev p-2 bg-[#fefefe] opacity-20 rounded-[50px]"><GrFormPrevious size={'20px'} /></div>
-    <div className="next p-2 bg-[#5454D4] rounded-[50px]"><GrFormNext className='icon-color text-[20px]' /></div>
-</div>
-     </div>
+        <h1 className="text-3xl font-bold py-10 pb-5 md:text-[48px]">Our Awesome Portfolio</h1>
+        <img className="ml-[90%] w-[34px]" src="./Circles.png" alt="circles" />
 
-      <img className=' w-[34px]' src='./Vector.png'/>
+        <div className="works py-5 m-auto ">
+          <div className="images mx-14 md:flex md:gap-20 md:mx-0 ">
+            {/* Display all images on larger screen */}
+            {images.map((image, index) => (
+              <img
+                key={index}
+                className={`md:block ${currentIndex === index ? '' : 'hidden'}`}
+                src={image}
+                alt={`carousel-${index}`}
+              />
+            ))}
+            {/* Display only one image on mobile screen */}
+            <img className=" hidden " src={images[currentIndex]} alt={`carousel-${currentIndex}`} />
+          </div>
+          <div className="controllers flex mt-10 justify-center gap-3 md:hidden">
+            <div className={`prev p-2 ${currentIndex === 0 ? 'bg-gray-300' : 'bg-[#fefefe] opacity-20'} rounded-[50px]`} onClick={handlePrevClick}>
+              <GrFormPrevious size={'20px'} />
+            </div>
+            <div className={`next p-2 ${currentIndex === images.length - 1 ? 'bg-gray-300' : 'bg-[#5454D4]'} rounded-[50px]`} onClick={handleNextClick}>
+              <GrFormNext className="icon-color text-[20px]" />
+            </div>
+          </div>
+        </div>
 
-
-
+        <img className="w-[34px] mt-10" src="./Vector.png" alt="vector" />
       </div>
-
-
-
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
